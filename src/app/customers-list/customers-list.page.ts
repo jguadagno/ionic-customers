7ae511583b82458eb0a231ca96d2e5c051eeb5f3
Joseph Guadagno <jguadagno@hotmail.com>
2019-01-17 05:47:35 -0700

    Step 3 - Post Delete

--- src/app/customers-list/customers-list.page.ts
+++ src/app/customers-list/customers-list.page.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { CustomerService } from '../services/customer.service';
 import { Customer } from '../services/customer.models';
 import { ApiResponse  } from '../services/api.response.model';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-customers-list',
@@ -12,7 +13,8 @@ import { ApiResponse  } from '../services/api.response.model';
 })
 export class CustomersListPage implements OnInit {
 
-  constructor(public http: HttpClient, public customerService: CustomerService) { }
+  constructor(public http: HttpClient, public customersService: CustomerService,
+    private alertController: AlertController) { }
 
   public customers: Customer[];
 
@@ -21,7 +23,31 @@ export class CustomersListPage implements OnInit {
   }
 
   public loadCustomers() {
-    this.customerService.getCustomers()
+    this.customersService.getCustomers()
       .subscribe((response: Customer[]) => { this.customers = response; });
+  }
+
+  public deleteCustomer(customer: Customer) {
+    this.customersService.deleteCustomer(customer)
+      .subscribe((response: ApiResponse) => {
+        if (response) {
+          this.displayAlert('Success', 'Successfully deleted the contact!');
+          if (response.wasSuccessful) {
+            this.loadCustomers();
+          }
+        }         else {
+          this.displayAlert('Failed', 'Failed to delete the contact!');
+          console.log('Failed to delete');
+        }
+      });
+  }
+
+  public async displayAlert(title: string, message: string) {
+    const alert = await this.alertController.create({
+      header: title,
+      message: message,
+      buttons: ['Ok']
+    });
+    await alert.present();
   }
 }
